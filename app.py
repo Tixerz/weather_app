74e4e39928c0b5726f7c9685b3bd0000a46ae0a9
Tixerz <petete1296@gmail.com>
2023-12-28 02:06:01 +0330

    basic gui

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 import requests 
 import geopy
-geolocator = geopy.Nominatim(user_agent="Weather")
+import tkinter as tk
+geolocator = geopy.Nominatim(user_agent="weather")
 api_key = '45db40e61c13b4a4c6609debf481e505'
-user_input = input("Enter the city: ")
-location = geolocator.geocode(user_input)
-weather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={location.latitude}&lon={location.longitude}&appid={api_key}')
-print(weather.json())
+root = tk.Tk()
+root.geometry('600x400')
+root.resizable(False , False)
+#search 
+
+
+search_entry = tk.Entry(root , width=40 )
+search_entry.place(x= 100 , y = 10)
+
+def search():
+    user_input = search_entry.get()
+    location = geolocator.geocode(user_input)
+    weather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={location.latitude}&lon={location.longitude}&appid={api_key}')
+    output = tk.Label(root ,text=str(int(weather.json()['main']['temp'])-273)+"°C" , font=('Helvetica bold',40))
+    output.place(x=250 , y= 100)
+    
+
+search_button = tk.Button(root , text="SEARCH" , command= search)
+search_button.place(x=440 , y= 10)
+root.mainloop()
